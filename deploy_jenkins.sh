0d2072914749060a3cfbd2749f4aaeae255f6812
Dongpp88 <77722434+Dongpp88@users.noreply.github.com>
2021-07-12 17:46:46 +0800

    Update deploy_jenkins.sh

--- deploy_jenkins.sh
+++ deploy_jenkins.sh
@@ -6,7 +6,7 @@ CONTAINER="jenkins_demo"
 IMAGE=$CONTAINER":"$(date -d "today" +"%Y%m%d_%H%M%S")
 
 # 删除滚动更新残留的容器
-docker rm `docker ps -a | grep -w $CONTAINER"_"$CONTAINER | awk '{print $1}'`
+docker rm webdemo
 # 强制删除滚动更新残留的镜像
 docker rmi --force `docker images | grep -w $CONTAINER | awk '{print $3}'`
 
